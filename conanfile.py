7e5ceb7aa7b71a70a73da3742f75c044f0cf237f
SSE4 <tomskside@gmail.com>
2018-06-09 14:51:46 +0700

    - first working prototype
    Signed-off-by: SSE4 <tomskside@gmail.com>

--- conanfile.py
+++ conanfile.py
@@ -1,83 +1,66 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from conans import ConanFile, CMake, tools
+from conans import ConanFile
 import os
 
 
-class LibnameConan(ConanFile):
-    name = "libname"
-    version = "0.0.0"
-    description = "Keep it short"
-    url = "https://github.com/bincrafters/conan-libname"
-    homepage = "https://github.com/original_author/original_lib"
-
-    # Indicates License type of the packaged library
+class GlibcVersionHeaderConan(ConanFile):
+    name = "glibc_version_header"
+    version = "0.1"
+    description = "Build portable Linux binaries without using an ancient distro"
+    url = "https://github.com/bincrafters/conan-glibc_version_header"
+    homepage = "https://github.com/wheybags/glibc_version_header"
     license = "MIT"
-
-    # Packages the license for the conanfile.py
     exports = ["LICENSE.md"]
-
-    # Remove following lines if the target lib does not use cmake.
-    exports_sources = ["CMakeLists.txt"]
-    generators = "cmake"
-
-    # Options may need to change depending on the packaged library.
-    settings = "os", "arch", "compiler", "build_type"
-    options = {"shared": [True, False], "fPIC": [True, False]}
-    default_options = "shared=False", "fPIC=True"
-
-    # Custom attributes for Bincrafters recipe conventions
+    options = {'glibc_version': [
+        '2.10.2',
+        '2.11.3',
+        '2.12.2',
+        '2.13',
+        '2.13.1',
+        '2.14',
+        '2.15',
+        '2.16',
+        '2.17',
+        '2.18',
+        '2.19',
+        '2.20',
+        '2.21',
+        '2.22',
+        '2.23',
+        '2.24',
+        '2.25',
+        '2.26',
+        '2.27',
+        '2.5.1',
+        '2.5',
+        '2.6.1',
+        '2.6',
+        '2.7',
+        '2.8',
+        '2.9'
+    ]}
+    default_options = 'glibc_version=2.12.2'
     source_subfolder = "source_subfolder"
-    build_subfolder = "build_subfolder"
-
-    # Use version ranges for dependencies unless there's a reason not to
-    # Update 2/9/18 - Per conan team, ranges are slow to resolve.
-    # So, with libs like zlib, updates are very rare, so we now use static version
-
-
-    requires = (
-        "OpenSSL/[>=1.0.2l]@conan/stable",
-        "zlib/1.2.11@conan/stable"
-    )
-
-    def config_options(self):
-        if self.settings.os == 'Windows':
-            del self.options.fPIC
+    no_copy_source = True
+    exports_sources = 'version_headers/*'
 
     def source(self):
-        source_url = "https://github.com/libauthor/libname"
-        tools.get("{0}/archive/v{1}.tar.gz".format(source_url, self.version))
-        extracted_dir = self.name + "-" + self.version
+        source_url = "https://github.com/wheybags/glibc_version_header.git"
+        self.run('git clone --depth 1 %s' % source_url)
 
-        #Rename to "source_subfolder" is a convention to simplify later steps
-        os.rename(extracted_dir, self.source_subfolder)
-
-    def configure_cmake(self):
-        cmake = CMake(self)
-        cmake.definitions["BUILD_TESTS"] = False # example
-        if self.settings.os != 'Windows':
-            cmake.definitions['CMAKE_POSITION_INDEPENDENT_CODE'] = self.options.fPIC
-        cmake.configure(build_folder=self.build_subfolder)
-        return cmake
-
-    def build(self):
-        cmake = self.configure_cmake()
-        cmake.build()
+        os.rename('glibc_version_header', self.source_subfolder)
 
     def package(self):
         self.copy(pattern="LICENSE", dst="licenses", src=self.source_subfolder)
-        cmake = self.configure_cmake()
-        cmake.install()
-        # If the CMakeLists.txt has a proper install method, the steps below may be redundant
-        # If so, you can just remove the lines below
-        include_folder = os.path.join(self.source_subfolder, "include")
-        self.copy(pattern="*", dst="include", src=include_folder)
-        self.copy(pattern="*.dll", dst="bin", keep_path=False)
-        self.copy(pattern="*.lib", dst="lib", keep_path=False)
-        self.copy(pattern="*.a", dst="lib", keep_path=False)
-        self.copy(pattern="*.so*", dst="lib", keep_path=False)
-        self.copy(pattern="*.dylib", dst="lib", keep_path=False)
+        self.copy(pattern="*.h", dst="version_headers", src=os.path.join(self.source_subfolder, 'version_headers'))
 
     def package_info(self):
-        self.cpp_info.libs = tools.collect_libs(self)
+        header_name = 'force_link_glibc_%s.h' % self.options.glibc_version
+        header_path = os.path.join(self.package_folder, 'version_headers', header_name)
+        self.cpp_info.cflags = ['-include %s' % header_path, '-static-libgcc']
+        self.cpp_info.cppflags = ['-include %s' % header_path, '-static-libgcc', '-static-libstdc++']
+
+    def package_id(self):
+        self.info.header_only()
