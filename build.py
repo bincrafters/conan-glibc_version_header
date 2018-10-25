5786868579efaac1c0e977886c6e06b383e99d79
Uilian Ries <uilianries@gmail.com>
2018-10-25 17:44:17 -0300

    Split package by version
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- build.py
+++ build.py
@@ -7,5 +7,8 @@ from bincrafters import build_template_header_only
 if __name__ == "__main__":
 
     builder = build_template_header_only.get_builder()
-
+    for version in ["2.10.2", "2.11.3", "2.12.2", "2.13", "2.14", "2.14.1", "2.15",
+                    "2.16", "2.17", "2.18", "2.19", "2.20", "2.21", "2.22", "2.23", "2.24",
+                    "2.25", "2.26", "2.27", "2.5.1", "2.5", "2.6.1", "2.6", "2.7", "2.8", "2.9"]:
+        builder.add(options={"glibc_version_header:glibc_version": version})
     builder.run()
--- conanfile.py
+++ conanfile.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from conans import ConanFile
 import os
+from conans import ConanFile, tools
 
 
 class GlibcVersionHeaderConan(ConanFile):
@@ -11,56 +11,56 @@ class GlibcVersionHeaderConan(ConanFile):
     description = "Build portable Linux binaries without using an ancient distro"
     url = "https://github.com/bincrafters/conan-glibc_version_header"
     homepage = "https://github.com/wheybags/glibc_version_header"
+    author = "Bincrafters <bincrafters@gmail.com>"
     license = "MIT"
     exports = ["LICENSE.md"]
-    options = {'glibc_version': [
-        '2.10.2',
-        '2.11.3',
-        '2.12.2',
-        '2.13',
-        '2.13.1',
-        '2.14',
-        '2.15',
-        '2.16',
-        '2.17',
-        '2.18',
-        '2.19',
-        '2.20',
-        '2.21',
-        '2.22',
-        '2.23',
-        '2.24',
-        '2.25',
-        '2.26',
-        '2.27',
-        '2.5.1',
-        '2.5',
-        '2.6.1',
-        '2.6',
-        '2.7',
-        '2.8',
-        '2.9'
+    settings = {"os_build": ["Linux"]}
+    options = {"glibc_version": [
+        "2.10.2",
+        "2.11.3",
+        "2.12.2",
+        "2.13",
+        "2.14",
+        "2.14.1",
+        "2.15",
+        "2.16",
+        "2.17",
+        "2.18",
+        "2.19",
+        "2.20",
+        "2.21",
+        "2.22",
+        "2.23",
+        "2.24",
+        "2.25",
+        "2.26",
+        "2.27",
+        "2.5.1",
+        "2.5",
+        "2.6.1",
+        "2.6",
+        "2.7",
+        "2.8",
+        "2.9"
     ]}
-    default_options = 'glibc_version=2.12.2'
-    source_subfolder = "source_subfolder"
+    default_options = {"glibc_version": "2.12.2"}
     no_copy_source = True
-    exports_sources = 'version_headers/*'
+    _source_subfolder = "source_subfolder"
+    _commit_id = "dd030738014620a1a0a0a49b58808914e79408ae"
+
+    @property
+    def header_name(self):
+        return "force_link_glibc_%s.h" % self.options.glibc_version
 
     def source(self):
-        source_url = "https://github.com/wheybags/glibc_version_header.git"
-        self.run('git clone --depth 1 %s' % source_url)
-
-        os.rename('glibc_version_header', self.source_subfolder)
+        tools.get("{}/archive/{}.zip".format(self.homepage, self._commit_id))
+        os.rename("{}-{}".format(self.name, self._commit_id), self._source_subfolder)
 
     def package(self):
-        self.copy(pattern="LICENSE", dst="licenses", src=self.source_subfolder)
-        self.copy(pattern="*.h", dst="version_headers", src=os.path.join(self.source_subfolder, 'version_headers'))
+        self.copy(pattern="LICENSE.TXT", dst="licenses", src=self._source_subfolder)
+        self.copy(pattern=self.header_name, dst="version_headers", src=os.path.join(self._source_subfolder, "version_headers"))
 
     def package_info(self):
-        header_name = 'force_link_glibc_%s.h' % self.options.glibc_version
-        header_path = os.path.join(self.package_folder, 'version_headers', header_name)
-        self.cpp_info.cflags = ['-include %s' % header_path, '-static-libgcc']
-        self.cpp_info.cppflags = ['-include %s' % header_path, '-static-libgcc', '-static-libstdc++']
-
-    def package_id(self):
-        self.info.header_only()
+        header_path = os.path.join(self.package_folder, "version_headers", self.header_name)
+        self.cpp_info.cflags = ["-include %s" % header_path, "-static-libgcc"]
+        self.cpp_info.cppflags = ["-include %s" % header_path, "-static-libgcc", "-static-libstdc++"]
